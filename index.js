f916f50882fa8e76c7d63d4545c23b77dc8af7c9
Craig Ford <craig_ford@icloud.com>
2022-02-23 16:13:26 -0500

    commit tree a388d698af84724577b742f8428739b515ba45b3

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('DOMContentLoaded is completed');
+})
+
+function handleError(error) {
+    document.body.innerHTML = error.message
+}
+
+function submitData(userName, userEMail) {
+
+    const fetchObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEMail,
+        }),
+    };
+
+
+    return fetch("http://localhost:3000/users", fetchObject)
+    .then(resp => resp.json()) 
+    .then(data =>  {
+        const newID  = parseInt(data.id)
+        document.body.innerHTML = `${newID}    ${data.name}     ${data.email}`
+    })
+    .catch(function (error) {
+        handleError(error)
+    })
+}
+
+
